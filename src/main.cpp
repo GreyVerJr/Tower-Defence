7b381c52781784085af0d48fd010b09888271a6e
Sergey <ssverbenko@miem.hse.ru>
2021-06-02 20:32:16 +0300

    Tower rotation

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include "Camera2D.hpp"
 #include "array"
+#include "cmath"
 #include "raylib-cpp.hpp"
 #include "raylib.h"
 
-struct Img {
+#define PI 3.14159265
+
+struct EnemyStruct {
     Vector2 origin = {0, 0};
     Rectangle sourceRec;
     Rectangle destRec;
     Texture2D texture;
     float rotation = 0;
+    int health = 100;
 };
+
+struct TowerStruct {
+    Vector2 origin = {0, 0};
+    Rectangle sourceRec;
+    Rectangle destRec;
+    Texture2D texture;
+    float rotation = 0;
+    const int radius = 300;
+};
+
+float distance(int x1, int y1, int x2, int y2) {
+    return (float)sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
 
 bool IsBuildingPossible(int PlatformPositionX, int PlatformPositionY,
                         int coins) {
@@ -21,6 +38,16 @@ bool IsBuildingPossible(int PlatformPositionX, int PlatformPositionY,
            coins >= 100;
 }
 
+float getRotation(int towerX, int towerY, int enemyX, int enemyY) {
+    float tg = (float)(enemyY - towerY) / (float)(enemyX - towerX);
+    float angle = atan(tg) * 180.0 / PI;
+    if ((enemyY - towerY) < 0 && (enemyX - towerX) < 0)
+        return angle - 180;
+    if ((enemyY - towerY) > 0 && (enemyX - towerX) < 0)
+        return angle + 180;
+    return angle;
+}
+
 int main(void) {
 
     // Initialization
@@ -30,11 +57,10 @@ int main(void) {
 
     InitWindow(screenWidth, screenHeight, "Tower Defence");
 
-    Img map;
-    Img tower;
-    Img enemy;
+    TowerStruct tower;
+    EnemyStruct enemy;
 
-    map.texture = LoadTexture("resources/map.png");
+    Texture2D mapTexture = LoadTexture("resources/map.png");
     tower.texture = (LoadTexture("resources/tower.png"));
     enemy.texture = (LoadTexture("resources/enemy.png"));
 
@@ -42,7 +68,7 @@ int main(void) {
                                           730, 885, 1040, 1040, 805};
     std::vector<int> PlatformPositionY = {500, 580, 345, 115, 115,
                                           425, 650, 270, 190, 115};
-    tower.rotation = 0;
+    tower.rotation = -90;
     // Source rectangle (part of the texture to use for drawing)
     tower.sourceRec = {0.0F, 0.0F, (float)tower.texture.width,
                        (float)tower.texture.height};
@@ -64,11 +90,14 @@ int main(void) {
     std::vector<int> waypointsY = {540, 540, 235, 235, 605, 605};
     std::vector<int> enemyRotation = {90, 180, -90, 180, 90, 180};
 
-    std::vector<Img> Towers;
-    for (int i = 0; i < 10; i++)
+    std::vector<int> waypointsCounter;
+    std::vector<TowerStruct> Towers;
+    for (int i = 0; i < 10; i++) {
         Towers.push_back(tower);
+        waypointsCounter.push_back(0);
+    }
 
-    std::vector<Img> Enemies;
+    std::vector<EnemyStruct> Enemies;
     for (int i = 0; i < 5; i++) {
         enemy.destRec = {920, (-60) * (float)(i + 1),
                          (float)enemy.texture.width * 2.0F,
@@ -81,8 +110,6 @@ int main(void) {
         IsPlatformFree.push_back(true);
 
     int coins = 0;
-    std::vector<int> waypointsCounter = {0, 0, 0, 0, 0};
-    // int previousPositionX
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
@@ -91,11 +118,10 @@ int main(void) {
     while (!WindowShouldClose()) { // Detect window close button or ESC key
                                    // Update
 
-        // previousPosition[0] = enemy.destRec.x;
-        // previousPosition[1] = enemy.destRec.y;
-
         coins++;
-        for (int i = 0; i < Enemies.size(); i++) { // Передвижение противников
+        // Передвижене противников
+        //---------------------------------------------------------------------------------
+        for (int i = 0; i < Enemies.size(); i++) {
             if (Enemies[i].destRec.x == waypointsX[waypointsCounter[i]] &&
                 Enemies[i].destRec.y == waypointsY[waypointsCounter[i]] &&
                 waypointsCounter[i] < waypointsX.size() - 1) {
@@ -111,14 +137,32 @@ int main(void) {
             if (Enemies[i].destRec.y > waypointsY[waypointsCounter[i]])
                 Enemies[i].destRec.y--;
         }
+        //---------------------------------------------------------------------------------
+
+        // Вращение башни
+        //---------------------------------------------------------------------------------
+        for (int i = 0; i < Towers.size(); i++) {
+            for (int j = 0; j < Enemies.size(); j++) {
+                if (distance((int)Towers[i].destRec.x, (int)Towers[i].destRec.y,
+                             (int)Enemies[j].destRec.x,
+                             (int)Enemies[j].destRec.y) <
+                    (float)Towers[0].radius) {
+                    Towers[i].rotation = getRotation(
+                        (int)Towers[i].destRec.x, (int)Towers[i].destRec.y,
+                        (int)Enemies[j].destRec.x, (int)Enemies[j].destRec.y);
+                    break;
+                }
+            }
+        }
+        //---------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        DrawTexture(map.texture, 0, 0, WHITE);
+        DrawTexture(mapTexture, 0, 0, WHITE);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < Towers.size(); i++) {
 
             if (IsBuildingPossible(PlatformPositionX[i], PlatformPositionY[i],
                                    coins)) {
@@ -138,16 +182,16 @@ int main(void) {
             }
         }
 
-        DrawText(TextFormat("Mouse position X: %03i", GetMouseX()), 10, 40, 20,
-                 LIGHTGRAY);
-        DrawText(TextFormat("Mouse position Y: %03i", GetMouseY()), 10, 80, 20,
-                 LIGHTGRAY);
-
         for (int i = 0; i < Enemies.size(); i++)
             if (Enemies[i].destRec.x != -20)
                 DrawTexturePro(Enemies[i].texture, enemy.sourceRec,
                                Enemies[i].destRec, Enemies[i].origin,
                                Enemies[i].rotation, WHITE);
+
+        DrawText(TextFormat("Box position X: %03i", GetMouseX()), 10, 40, 20,
+                 LIGHTGRAY);
+        DrawText(TextFormat("Box position Y: %03i", GetMouseY()), 10, 80, 20,
+                 LIGHTGRAY);
 
         EndDrawing();
         //---------------------------------------------------------------------------------
